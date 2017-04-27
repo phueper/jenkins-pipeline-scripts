635bf303c35a681b068ea0e19bd66270cc6c0d89
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-04-27 22:20:48 +0200

    Fix default params

--- vars/runDockerCompose.groovy
+++ vars/runDockerCompose.groovy
@@ -2,11 +2,13 @@ import java.util.UUID
 
 def call(
         String composeFileSuffix = '',
-        String uuid = UUID.randomUUID(),
+        String composeProjectName = UUID.randomUUID(),
         Closure body = null
 ) {
+    if (!composeFileSuffix) composeFileSuffix = ''
+    if (!composeProjectName) composeProjectName = UUID.randomUUID()
     withEnv(["COMPOSE_FILE=docker-compose${composeFileSuffix}.yml",
-             "COMPOSE_PROJECT_NAME=${uuid}"]) {
+             "COMPOSE_PROJECT_NAME=${composeProjectName}"]) {
         ansiColor('xterm') {
             try {
                 sh "docker-compose config && docker-compose up --abort-on-container-exit"
@@ -21,5 +23,5 @@ def call(
 }
 
 def call(args, Closure body = null) {
-    return call(args.composeFileSuffix, args.uuid, body)
+    return call(args.composeFileSuffix, args.composeProjectName, body)
 }

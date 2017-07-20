2f882fb09e535d59ff60379d7445feb08fb920e9
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-07-20 11:12:53 +0200

    Try with Serializable and NonCPS

--- vars/labels.groovy
+++ vars/labels.groovy
@@ -1,6 +1,7 @@
 import jenkins.model.*
 
-class labels {
+class labels extends Serializable {
+    @NonCPS
     boolean isOnNodeWithLabel(String label) {
         Jenkins.instance.getLabels().contains(Jenkins.instance.getLabelAtom(label))
     }

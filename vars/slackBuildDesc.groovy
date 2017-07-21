f182e761abef6c5200b30542576882db8915bf20
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-07-21 15:32:26 +0200

    Use env.GIT_COMMIT

--- vars/slackBuildDesc.groovy
+++ vars/slackBuildDesc.groovy
@@ -1,6 +1,6 @@
 String call() {
     def githubUrl = sh(returnStdout: true, script: 'git config --get remote.origin.url').trim().replaceFirst(/\.git$/, '')
-    def commit = gitCommit()
+    def commit = env.GIT_COMMIT == null ? gitCommit() : env.GIT_COMMIT
     def jobUrl = env.JOB_DISPLAY_URL == null ? env.JOB_URL : env.JOB_DISPLAY_URL
     def runUrl = env.RUN_DISPLAY_URL == null ? currentBuild.absoluteUrl : env.RUN_DISPLAY_URL
     return "<${jobUrl}|${env.JOB_NAME}> <${runUrl}|${currentBuild.displayName}> for <${githubUrl}/commit/${commit}|${commit.take(6)}>".toString()

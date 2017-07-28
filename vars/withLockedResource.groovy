4d4b1dd79269091d76c40ddb2b3a019ad7a822d3
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-07-28 21:09:07 +0200

    Export name of lockedResource as env.LOCKED_RESOURCE

--- vars/withLockedResource.groovy
+++ vars/withLockedResource.groovy
@@ -1,5 +1,23 @@
+import java.util.Collection
+import org.jenkins.plugins.lockableresources.LockableResourcesManager
+import org.jenkins.plugins.lockableresources.LockableResource
+
+Collection<LockableResource> getLockedResources() {
+    LockableResourcesManager.class.get().getResourcesFromBuild(currentBuild.getRawBuild())
+}
+
 def call(String label, Closure body) {
+    Collection<LockableResource> previous = getLockedResources()
+    env.LOCKED_RESOURCE = null
     lock(label: label, quantity: 1) {
+        Collection<LockableResource> current = getLockedResources()
+        current.removeAll(previous)
+        for (LockableResource res : current) {
+            if (res.isValidLabel(label, null)) {
+                env.LOCKED_RESOURCE = res.getName()
+            }
+        }
+
         return body()
     }
 }

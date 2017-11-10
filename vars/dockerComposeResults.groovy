571c20c357f7bd56ebd01dc6e7ece94cb0415230
Marcus Mennemeier <marcus.mennemeier@actano.de>
2017-11-10 11:19:19 +0100

    add docker tool to results

--- vars/dockerComposeResults.groovy
+++ vars/dockerComposeResults.groovy
@@ -4,21 +4,23 @@ def call(
         boolean allowEmptyResults = false,
         double healthScaleFactor = 1.0
 ) {
-    CONTAINER=sh(returnStdout: true, script: "docker-compose ps -q ${service}").trim()
-    exit_code=sh(returnStdout: true, script: "docker wait ${CONTAINER}").trim()
-    if (testReportsDir != null) {
-        copyExitCode = sh(returnStatus: true, script: $/
+    docker.withTool('docker') {
+        CONTAINER = sh(returnStdout: true, script: "docker-compose ps -q ${service}").trim()
+        exit_code = sh(returnStdout: true, script: "docker wait ${CONTAINER}").trim()
+        if (testReportsDir != null) {
+            copyExitCode = sh(returnStatus: true, script: $/
 docker cp ${CONTAINER}:${testReportsDir}/ ./results
 SED_COMMAND="s:$(echo -n "[[ATTACHMENT|${testReportsDir}" | sed 's/[\[\:&.*]/\\&/g'):$(echo -n "[[ATTACHMENT|$(pwd)/results" | sed 's/[:&]/\\&/g'):g"
 find ./results -name '*.xml' -exec sed -i "$${SED_COMMAND}" {} \;
 /$)
-        if (copyExitCode != 0) {
-            echo("WARNING: Fetching test results from container failed with ${copyExitCode}")
+            if (copyExitCode != 0) {
+                echo("WARNING: Fetching test results from container failed with ${copyExitCode}")
+            }
+            junit(testResults: 'results/**/*.xml', allowEmptyResults: allowEmptyResults, healthScaleFactor: healthScaleFactor, testDataPublishers: [[$class: 'AttachmentPublisher']])
         }
-        junit(testResults: 'results/**/*.xml', allowEmptyResults: allowEmptyResults, healthScaleFactor: healthScaleFactor, testDataPublishers: [[$class: 'AttachmentPublisher']])
-    }
-    if (exit_code != "0") {
-        error("Service ${service} exited with code ${exit_code}")
+        if (exit_code != "0") {
+            error("Service ${service} exited with code ${exit_code}")
+        }
     }
 }
 
